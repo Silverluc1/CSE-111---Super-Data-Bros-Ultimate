fb7400a80f81cb0b6a4f5a55d296565c24b53788
Lucas Tran <lucastran22@gmail.com>
2021-11-09 17:44:43 -0800

    tessting first commit to edit on both ends

--- SuperDataBrosUltimate.sql
+++ SuperDataBrosUltimate.sql
@@ -1,1 +1,15 @@
 --This file will have the creation of the main SQL commands that would extract the data for display.
+
+CREATE TABLE Attack(
+
+    smashleft long,
+    smashright long,
+    smashUp long,
+    smashDown long
+)
+
+CREATE TABLE Movement(
+    leftright long,
+    updown long,
+)
+;
