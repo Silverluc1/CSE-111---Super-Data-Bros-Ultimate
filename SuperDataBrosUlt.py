5f5ad810f66306b0ad45d1afe18571933230960c
Lucas Tran <lucastran22@gmail.com>
2021-11-30 18:05:50 -0800

    new and idea for implementing many relationship

---
+++ SuperDataBrosUlt.py
@@ -0,0 +1,7 @@
+import sqlite3
+from sqlite3 import Error
+
+#this file is going to be the "front end" of the project.
+#it is going to be interactable through the terminal where 
+#the user will get to do everything
+
