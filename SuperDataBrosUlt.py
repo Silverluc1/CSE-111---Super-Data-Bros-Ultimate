e037cb43c523fac75eea5ee4dcaf4a38872b67a5
Lucas Tran <lucastran22@gmail.com>
2021-12-02 18:03:37 -0800

    checking user input within menu selection range

--- SuperDataBrosUlt.py
+++ SuperDataBrosUlt.py
@@ -7,10 +7,19 @@ from sqlite3 import Error
 
 print('Welcome to Our Application Called: Super Data Bros Ultimate! \n \n')
 print('We have gathered data of your 10 most favorite characters from the fan-favorite game!\n')
-print('To get started what would you like to do first? We have the options below: \n \n \n')
+print('To get started what would you like to do first? We have the options below: \n \n')
 print('Press 1 for Character Data and Facts\n')
 print('Press 2 for Favorited Characters\n')
 print('Press 3 for Personal Notes\n \n')
 
-userInput = input('Please Enter In Your Selection: ')
-print('ECHOING: ', userInput)
+userInputText = input('Please Enter In Your Selection: ')
+userInputNum = int(userInputText)
+
+while( True ):
+    if (not(1 <= userInputNum < 4)):
+        userInputText = input('Please enter a number 1-3: ')
+        userInputNum = int(userInputText)
+    else:
+        break
+
+
